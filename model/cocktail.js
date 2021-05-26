1e32c99c35e0b9f74aeefee9f4fd6e01f603d945
Krystian <chwaja.k03@htlwienwest.at>
2021-05-26 13:20:19 +0200

    Aufgabe 5 Erledigt

--- model/cocktail.js
+++ model/cocktail.js
@@ -8,6 +8,24 @@ async function getCocktailsAll() {
   };
 }
 
-module.exports = {
-    getCocktailsAll,
+async function getCocktailZutaten(cname) {
+  const { rows } = await db.query(
+    'SELECT zbez FROM cocktail JOIN besteht b ON cocktail.cid = b.cid JOIN zutat z ON b.zid = z.zid WHERE cname = $1',
+    [cname],
+  );
+  if (rows.length > 0) {
+    return {
+      code: 200,
+      data: rows,
+    };
+  }
+  return {
+    code: 404,
+    data: `the specified cocktail with the Name ${cname} was not found in the database`,
   };
+}
+
+module.exports = {
+  getCocktailsAll,
+  getCocktailZutaten,
+};
--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ const asyncHandler = require('express-async-handler');
 const router = express.Router();
 const {
   getCocktailsAll,
+  getCocktailZutaten
 } = require('../model/cocktail');
 
 router.get(
@@ -14,4 +15,11 @@ router.get(
   }),
 );
 
+router.get(
+    '/cocktails/:cname/zutaten',
+    asyncHandler(async (req, res) => {
+      const result = await getCocktailZutaten(req.params.cname);
+      res.status(result.code).json(result);
+    }),
+  );
 module.exports = router;
